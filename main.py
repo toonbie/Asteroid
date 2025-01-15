cff9ac3a09a4accedac0d901b80687c745074698
toonbie <milanburny@gmail.com>
2025-01-15 18:04:05 +0000

    Added clock locked at 60fps

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,5 +14,6 @@ def main():
 				return
 		screen.fill((0,0,0))
 		pygame.display.flip()
+		dt = clock.tick(60)/1000
 if __name__ == "__main__":
 	main()
