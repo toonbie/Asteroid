db66f147c13fc0b7c5cf428931349c42ee6e2f82
toonbie <milanburny@gmail.com>
2025-01-16 18:01:11 +0000

    Can move forward & back

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED , PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -19,11 +19,19 @@ class Player(CircleShape):
         pygame.draw.polygon(screen,(255,255,255),self.triangle(),2)
 
     def rotate(self, dt):
-        self.rotation += PLAYER_TURN_SPEED * dt 
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
