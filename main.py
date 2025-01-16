172640206a93db6b878e6da5a4595793656fe460
toonbie <milanburny@gmail.com>
2025-01-16 18:16:18 +0000

    Put player updates into groups

--- main.py
+++ main.py
@@ -8,16 +8,25 @@ def main():
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable,drawable)
 	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		player.update(dt)
+		for updates in updatable:
+			updates.update(dt)
+
 		screen.fill((0,0,0))
-		player.draw(screen)
+
+		for draw in drawable:
+			draw.draw(screen)
+
 		pygame.display.flip()
 		dt = clock.tick(60)/1000
+
 if __name__ == "__main__":
 	main()
