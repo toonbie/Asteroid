b37b097d86b2cf061e2b6572881dfb7043a6c98c
toonbie <milanburny@gmail.com>
2025-01-16 22:27:46 +0000

    Players can shoot bullets

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 def main(): 
 	pygame.init()
 	clock = pygame.time.Clock()
@@ -13,9 +14,11 @@ def main():
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
 	Player.containers = (updatable,drawable)
 	Asteroid.containers =(asteroids,updatable,drawable)
 	AsteroidField.containers=(updatable)
+	Shot.containers=(updatable,drawable)
 	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	asteroidfield = AsteroidField()
@@ -31,6 +34,10 @@ def main():
 		for draw in drawable:
 			draw.draw(screen)
 		for asteroid in asteroids:
+			for shot in shots:
+				if shot.collision(asteroid):
+					print("hit")
+
 			if player.collision(asteroid):
 				print("Game Over!")
 				exit()
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED , PLAYER_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED , PLAYER_SPEED , PLAYER_SHOOT_SPEED
 import pygame
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -25,7 +26,10 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
-    
+    def shoot(self):
+        bullet = Shot(self.position[0],self.position[1])
+        bullet.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
@@ -36,3 +40,5 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
